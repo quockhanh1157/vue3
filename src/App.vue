85a70dea4de75037f68f29f19b9874bb32d0e011
KhanhNguyen <nguyen.quoc.khanh.531@gmail.com>
2023-06-26 14:55:09 +0700

    create form default

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
-  <button v-text="changeButton ? optionsAPI : compositionAPI"
-          @click.prevent="changeButton = !changeButton"
-  ></button>
-
-  <SignupFormOptionsAPI v-if="changeButton"/>
-  <SignupFormCompositionAPI v-if="!changeButton"/>
-
+  <p>Page default</p>
 </template>
 
 <script setup>
-import SignupFormOptionsAPI from "@/components/SignupFormOptionsAPI.vue";
-import {ref} from "vue";
-import SignupFormCompositionAPI from "@/components/SignupFormCompositionAPI.vue";
 
-const changeButton = ref(false)
-const optionsAPI = ref("Options API")
-const compositionAPI = ref("Composition API")
 </script>
 
 <style>
@@ -23,13 +11,4 @@ body {
   background-color: #555555;
 }
 
-button {
-  background: #3c48b6;
-  border: 0;
-  padding: 10px 20px;
-  color: white;
-  border-radius: 20px;
-  cursor: pointer;
-  margin: auto;
-}
 </style>
