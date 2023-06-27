6a7829022878c89442f3acc81fb6c1fba3cb85e7
KhanhNguyen <nguyen.quoc.khanh.531@gmail.com>
2023-06-27 10:26:49 +0700

    data

--- src/views/jobs/JobDetail.vue
+++ src/views/jobs/JobDetail.vue
@@ -1,15 +1,39 @@
 <template>
-  <p>Jobs Detail Page with id props: {{id}} - id Url: {{idURl}}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>Jobs Detail Page with id props: {{ id }} - id Url: {{ idURl }}</p>
+
+    <p>{{ job.detail }}</p>
+  </div>
+  <div v-else>
+    <Loading>
+      <template v-slot:detail>
+        <p>Loading for detail...</p>
+      </template>
+    </Loading>
+  </div>
 </template>
 
 <script>
+import Loading from "@/components/Loading.vue";
+
 export default {
   name: "JobDetail",
-  props:['id'],
-  data(){
-    return{
-      idURl:this.$route.params.id
+  components: {Loading},
+  props: ['id'],
+  data() {
+    return {
+      idURl: this.$route.params.id,
+      job: null
     }
+  },
+  mounted() {
+    setTimeout(()=>{
+      fetch(`http://localhost:3000/jobs/${this.id}`)
+          .then((res) => res.json())
+          .then((data) => this.job = data)
+          .catch((err) => console.log(err.message))
+    },5000)
   }
 }
 </script>
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,26 +1,41 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <router-link :to="{
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{
       name:'jobDetail',
       params:{id:job.id}}"
-    >
-      {{ job.detail }}
-    </router-link>
+      >
+        {{ job.detail }}
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <Loading>
+      <template v-slot:detail>
+        <p>Loading for jobs...</p>
+      </template>
+    </Loading>
   </div>
 </template>
 
 <script>
+import Loading from "@/components/Loading.vue";
 export default {
   name: "Jobs",
+  components: {Loading},
   data() {
     return {
-      jobs: [
-        {title: "FrontEnd", id: 1, detail: "FrontEnd"},
-        {title: "BackEnd", id: 2, detail: "BackEnd"},
-        {title: "FullStack", id: 3, detail: "FullStack"}
-      ]
+      jobs: []
     }
+  },
+  mounted() {
+    setTimeout(()=>{
+      fetch("http://localhost:3000/jobs")
+          .then((res) => res.json())
+          .then((data) => this.jobs = data)
+          .catch((err) => console.log(err.message))
+    },5000)
   }
 }
 </script>
